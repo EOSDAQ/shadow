d9f655a380552ec092434c3785bb24178603beb9
newro <newro@mtcom.co.kr>
2018-08-07 10:37:40 +0900

    local_keys 키 저장 위치 export

--- local_keys.sh
+++ local_keys.sh
@@ -3,34 +3,36 @@
 # Local eos
 alias cleos='docker exec local_eosio /opt/eosio/bin/cleos --wallet-url http://localhost:8888'
 export EOSIOKEY=5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+export LOCAL_EOS=`cat ~/.local_eos`
 
 openwallet () { 
-    grep PW5 .$1 | sed 's/"//g' 
+    grep PW5 ${LOCAL_EOS}/.$1 | sed 's/"//g' 
 }
 pubkey () { 
-    grep Public .$1 | sed 's/Public key: //g' 
+    grep Public ${LOCAL_EOS}/.$1 | sed 's/Public key: //g' 
 }
 prikey() { 
-    grep Private .$1 | sed 's/Private key: //g' 
+    grep Private ${LOCAL_EOS}/.$1 | sed 's/Private key: //g' 
 }
 
 if [[ "$@" == "new" ]]; then
     echo "generation new wallet&key"
 
     rm -f ./local_data/*.wallet
+    echo `pwd` > ~/.local_eos
 
     # wallet gen
-    cleos wallet create 2>&1 > .master
-    cleos wallet create -n wall1 2>&1 > .wall1
-    cleos wallet create -n wall2 2>&1 > .wall2
-    cleos wallet create -n wall3 2>&1 > .wall3
-    cleos wallet create -n wall4 2>&1 > .wall4
+    cleos wallet create 2>&1 > ${LOCAL_EOS}/.master
+    cleos wallet create -n wall1 2>&1 > ${LOCAL_EOS}/.wall1
+    cleos wallet create -n wall2 2>&1 > ${LOCAL_EOS}/.wall2
+    cleos wallet create -n wall3 2>&1 > ${LOCAL_EOS}/.wall3
+    cleos wallet create -n wall4 2>&1 > ${LOCAL_EOS}/.wall4
 
     # key gen
-    cleos create key 2>&1 > .key1
-    cleos create key 2>&1 > .key2
-    cleos create key 2>&1 > .key3
-    cleos create key 2>&1 > .key4
+    cleos create key 2>&1 > ${LOCAL_EOS}/.key1
+    cleos create key 2>&1 > ${LOCAL_EOS}/.key2
+    cleos create key 2>&1 > ${LOCAL_EOS}/.key3
+    cleos create key 2>&1 > ${LOCAL_EOS}/.key4
 
     cleos wallet import --private-key ${EOSIOKEY}
     cleos wallet import -n wall1 --private-key `prikey key1`
